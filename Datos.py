3eb11dacf9808c64b7ed90d29d6666f9fe312008
Ivan Aldana <ialdanacon>
2025-11-21 15:15:06 -0500

    Feat(Practica_datos): Aqui agregamos agregamos el de los get y los setter, donde reciben los nombre, los apellidos etc..

--- Datos.py
+++ Datos.py
@@ -1,13 +1,13 @@
-from typing import List
-class Datos:
-    def __init__(self):
-        pass
+#Aqui iniciamos el bloque del get y setter
+class Datos: 
+    def __init__(self): 
+        pass 
 
-    @property
+    @property 
     def Nombre(self)-> str:
-        return self._Nombre
+        return self._Nombre 
     
-    @Nombre.setter
+    @Nombre.setter 
     def Nombre(self, Nombre: str):
         self._Nombre = Nombre
     
@@ -27,7 +27,14 @@ class Datos:
     def Edad(self, edad: int):
         self._Edad = edad
     
-
+    @property
+    def Cedula(self)-> int:
+        return self._Cedula
+    
+    @Cedula.setter
+    def Cedula(self, cedula: int):
+        self._Cedula = cedula
+    
     @property
     def Correo(self)-> str:
         return self._correo
@@ -35,4 +42,4 @@ class Datos:
     @Correo.setter
     def Correo(self, correo: str):
         self._correo = correo
-
+#Aqui finalisamos el bloque de get t setter
