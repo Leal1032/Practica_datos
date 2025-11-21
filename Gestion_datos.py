9c7bc80f14d0e72fe0b65587e04e9ea3c7f85aa5
Ivan Aldana <ialdanacon>
2025-11-21 15:16:21 -0500

    Feat(Practica_datos): Aqui creamos dos funciones una para agregar los datos del menu a la lista y para consultar el usuario por numero de identidad

--- Gestion_datos.py
+++ Gestion_datos.py
@@ -1,23 +1,15 @@
 from Datos import Datos
 from typing import List
-
+#Aqui creamos una clase hija para la lista y la consulta
 class GestionDatos:
     def __init__(self):
         self.datos_list: List[Datos] = []
 
+    def agregar_datos(self, datos: Datos):
+        self.datos_list.append(datos)
 
-    @property
-    def ListaDatos(self)-> List:
-        return self._ListaDatos
-    
-    @ListaDatos.setter
-    def ListaDatos(self,ListaDatos:List):
-        self._ListaDatos=ListaDatos
-
-        
-    def mostrarDatos(self):
-        for datos in self.ListaDatos:
-            print("El nombre de la persona es: ",self.Nombre)
-            print("El apellido de la persona es: ",self.Apellido)
-            print("La edad de la persona es: ",self.Edad)
-            print("El correo de la persona es: ",self.Correo)
+    def consultar_por_cedula(self, cedula: int):
+        for datos in self.datos_list:
+            if datos.Cedula == cedula:
+                return datos
+        return None
